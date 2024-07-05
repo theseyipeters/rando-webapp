22f06bf62389bbff1dbb9238d4a4b7cd219a08a2
devseyipeters <seyi@usecrafter.com>
2024-07-05 18:58:06 +0100

    Add Provider Context.

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,29 @@ import axios from "axios";
 import Home from "./pages/home/Home";
 import Generate from "./pages/generate/Generate";
 import Login from "./pages/auth/login/Login";
+import { ProviderProvider } from "../src/context/ProviderContext";
 
 function App() {
 	return (
 		<div>
-			<BrowserRouter>
-				<Routes>
-					<Route
-						path="/"
-						element={<Home />}
-					/>
-					<Route
-						path="/generate"
-						element={<Generate />}
-					/>
-					<Route
-						path="/auth/login"
-						element={<Login />}
-					/>
-				</Routes>
-			</BrowserRouter>
+			<ProviderProvider>
+				<BrowserRouter>
+					<Routes>
+						<Route
+							path="/"
+							element={<Home />}
+						/>
+						<Route
+							path="/generate"
+							element={<Generate />}
+						/>
+						<Route
+							path="/auth/login"
+							element={<Login />}
+						/>
+					</Routes>
+				</BrowserRouter>
+			</ProviderProvider>
 		</div>
 	);
 }
